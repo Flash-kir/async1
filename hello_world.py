88d78ffc83d8a540fc107293b0b03285effc836d
Flash-kir <flash-kir@yandex.ru>
2024-04-17 11:27:58 +0300

    add async function

--- hello_world.py
+++ hello_world.py
@@ -1,24 +1,40 @@
+import asyncio
 import time
 import curses
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
 def draw(canvas):
     canvas.border()
     curses.curs_set(False)
-    row, column = (5, 20)
+    star = blink(canvas, 5, 20)
     while True:
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        canvas.refresh()
-        time.sleep(2)
-        canvas.addstr(row, column, '*')        
-        canvas.refresh()
-        time.sleep(0.3)
-        canvas.addstr(row, column, '*', curses.A_BOLD)
+        star.send(None)
         canvas.refresh()
         time.sleep(0.5)
-        canvas.addstr(row, column, '*')        
+        star.send(None)
         canvas.refresh()
-        time.sleep(0.3)
+        time.sleep(0.5)
+        star.send(None)
+        canvas.refresh()
+        time.sleep(0.5)
+        star.send(None)
+        canvas.refresh()
+        time.sleep(0.5)
 
 
 if __name__ == '__main__':
