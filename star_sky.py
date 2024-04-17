942d13fcef187e5784f5dab5a526c120128b8c72
Flash-kir <flash-kir@yandex.ru>
2024-04-17 22:21:19 +0300

    add space ship animation

--- star_sky.py
+++ star_sky.py
@@ -3,6 +3,10 @@ import time
 import curses
 import random
 import os
+from itertools import cycle
+from curses_tools import draw_frame
+from curses_tools import get_frame_size
+from curses_tools import read_controls
 
 
 async def blink(canvas, row, column, symbol='*'):
@@ -54,11 +58,54 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         column += columns_speed
 
 
+async def animate_spaceship(canvas, start_row, start_column, frames):
+    last_frame = ''
+    row_delta = 0
+    col_delta = 0
+    for frame in cycle(frames):
+        if last_frame:
+            draw_frame(
+                canvas,
+                start_row - row_delta,
+                start_column - col_delta,
+                last_frame,
+                negative=True
+            )
+        ship_height, ship_width = get_frame_size(frame)
+        row_delta = round(ship_height/2)
+        col_delta = round(ship_width/2)
+        draw_frame(
+            canvas,
+            start_row - row_delta,
+            start_column - col_delta,
+            frame)
+        last_frame = frame
+        for i in range(random.randint(2, 4)):
+            await asyncio.sleep(0)
+
+
+def load_space_ship_frame(file_path):
+    with open(file_path, "r") as my_file:
+        file_contents = my_file.read()
+    return file_contents
+
+
 def draw(canvas):
+    space_ship_frame_1 = load_space_ship_frame('frames/rocket_frame_1.txt')
+    space_ship_frame_2 = load_space_ship_frame('frames/rocket_frame_2.txt')
+    frames = [space_ship_frame_1, space_ship_frame_2]
+
     star_symbols = ['*', ':', '+', '.']
     (col_max, row_max) = os.get_terminal_size(0)
     canvas.border()
     curses.curs_set(False)
+
+    space_ship_corutine = animate_spaceship(
+        canvas,
+        round(row_max/2),
+        round(col_max/2),
+        frames
+    )
 
     fire_corutine = fire(
         canvas,
@@ -73,7 +120,7 @@ def draw(canvas):
         except StopIteration:
             break
 
-    coroutines = []
+    coroutines = [space_ship_corutine]
     for a in range(200):
         star_row = random.randint(1, row_max - 2)
         star_col = random.randint(1, col_max - 2)
